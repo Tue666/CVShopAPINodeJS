e6eab9430f4ac76dfb1165cf8a08ba70e56f27a6
Tue666 <dapamu333@gmail.com>
2021-11-28 09:31:48 +0700

    update mongoose without env value

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.ATLAS_URI, () => {
+        await mongoose.connect('mongodb+srv://pihe:lmht292001@cluster0.vviso.mongodb.net/cv_shop?retryWrites=true&w=majority', () => {
             console.log('DB connection successful');
         });
     } catch (error) {
