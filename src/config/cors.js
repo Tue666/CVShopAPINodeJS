315ed02824363eb68d5bed0f206d74f7acf6d398
Tue666 <dapamu333@gmail.com>
2021-12-01 21:59:52 +0700

    edit multiple access domain for cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,11 @@
 const cors = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://dot-shop-reactjs.herokuapp.com, http://localhost:3000');
+    const allowedOrigins = ['https://dot-shop-reactjs.herokuapp.com', 'http://localhost:3000'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    
+
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
         return res.status(200).json({});
