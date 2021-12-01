c39d30686b9ad822979f50a72abbc86c9475b924
Tue666 <dapamu333@gmail.com>
2021-12-01 22:09:06 +0700

    update cors  access domain to *

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,11 +1,7 @@
 const cors = (req, res, next) => {
-    const allowedOrigins = ['https://dot-shop-reactjs.herokuapp.com', 'http://localhost:3000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
+    
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
         return res.status(200).json({});
